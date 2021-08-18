5b0aa1aed145a709644b364054b93885267f9dff
Luciana Sugeta <lulusugeta@gmail.com>
2021-08-18 17:15:21 -0400

    commit tree 00e1fe0d914e63aa4f834a150be058021e41eb0d

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function(popUp) {
+    alert('Dangerous! Your computer is being hacked!!');
+    });
 }
+addingEventListener();
